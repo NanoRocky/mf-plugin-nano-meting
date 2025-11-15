a695eba52c14abf072a28c931d269075f09248dd
Pizero <zhaory200707@outlook.com>
2025-11-15 17:03:52 +0800

    feat: support search & basic typing

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,58 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
-import cryptojs from 'crypto-js';
-import dayjs from 'dayjs';
-import bigInt from 'big-integer';
-import he from 'he';
-import qs from 'qs';
-
-// TODO: 你可以在这里写插件的逻辑
-
-// 注意：不要使用async () => {}，hermes不支持异步箭头函数
-const search: IPlugin.ISearchFunc = async function (query, page, type) {
-  if(type === 'music') {
-    return {
-      isEnd: true,
-      data: []
-    }
-  };
-}
-
-
-const pluginInstance: IPlugin.IPluginDefine = {
-  platform: "插件名",
-  version: "0.0.0",
-  // TODO: 在这里把插件剩余的功能补充完整
-  search
+const NANO_METING = "https://metingapi.nanorocky.top/";
+const buildUrl = (query: NanoMeting.SearchParams) => {
+  const url = new URL(NANO_METING);
+  Object.entries(query).reduce((acc, [k, v]) => {
+    acc.append(k, v);
+    return acc;
+  }, url.searchParams);
+  return url;
 };
 
-
-export default pluginInstance;
+async function search<T extends IMedia.SupportMediaType>(
+  query: string,
+  page: number,
+  type: T,
+) {
+  return type !== "music"
+    ? ({ isEnd: true, data: [] } as IPlugin.ISearchResult<"music">)
+    : await fetch(
+        buildUrl({
+          server: "netease",
+          type: "search",
+          id: "0",
+          keyword: query,
+        }),
+      )
+        .then((i) => i.json())
+        .then(
+          (i) =>
+            ({
+              isEnd: true,
+              data: (i as NanoMeting.SearchRsp).map((i) => ({
+                artist: i.artist,
+                title: i.name,
+                album: i.album,
+                artwork: i.pic,
+                url: i.url,
+                lrc: i.lrc,
+                platform: i.sourse,
+                id: new URL(i.url).searchParams.get("id"),
+              })),
+            }) as IPlugin.ISearchResult<"music">,
+        )
+        .catch(
+          () => ({ isEnd: true, data: [] }) as IPlugin.ISearchResult<"music">,
+        );
+}
+export default {
+  platform: "nano-meting",
+  author: "Pizero",
+  // TODO: update
+  // srcUrl: "https://example.catcat.work/xxx.js",
+  // primaryKey: ["id", "aid", "bid"],
+  cacheControl: "cache",
+  version: "0.0.0",
+  supportedSearchType: ["music"],
+  // TODO: 在这里把插件剩余的功能补充完整
+  search,
+} as IPlugin.IPluginDefine;
---
+++ types/nanoMeting.d.ts
@@ -0,0 +1,31 @@
+declare namespace NanoMeting {
+  type Provider = "netease" | "tencent";
+  // TODO: more bound
+  interface SearchParams {
+    server: Provider;
+    type:
+      | "name"
+      | "artist"
+      | "url"
+      | "pic"
+      | "lrc"
+      | "song"
+      | "playlist"
+      | "search";
+    id: string;
+    picsize?: number;
+    keyword?: string;
+    dwrc?: boolean | "open";
+    trlrc?: boolean | "only";
+  }
+  interface SearchPspItem {
+    name: string;
+    artist: string;
+    album: string;
+    url: string;
+    pic: string;
+    lrc: string;
+    sourse: Provider;
+  }
+  type SearchRsp = SearchPspItem[];
+}
