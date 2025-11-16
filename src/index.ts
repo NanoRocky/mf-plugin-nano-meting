aa62dbecfad85a276945f5fbc013c396bccc10b1
Pizero <zhaory200707@outlook.com>
2025-11-16 18:06:51 +0800

    feat: support tencent

--- src/index.ts
+++ src/index.ts
@@ -8,48 +8,78 @@ const buildUrl = (query: NanoMeting.SearchParams) => {
   return url;
 };
 
+async function searchByProvider(query: string, server: NanoMeting.Provider) {
+  try {
+    const iData = await fetch(
+      buildUrl({
+        server,
+        type: "search",
+        id: "0",
+        keyword: query,
+      }),
+      {
+        headers: {
+          Referer:
+            query === "netease"
+              ? undefined
+              : "https://metingapi.nanorocky.top/",
+        },
+      },
+    );
+    const i = await iData.json();
+    const o = {
+      isEnd: true,
+      data: (i as NanoMeting.SearchRsp).map((i) => ({
+        artist: i.artist,
+        title: i.name,
+        album: i.album,
+        artwork: i.pic,
+        url: i.url,
+        lrc: i.lrc,
+        platform: server,
+        id: new URL(i.url).searchParams.get("id"),
+      })),
+    } as IPlugin.ISearchResult<"music">;
+    return o;
+  } catch {
+    return { isEnd: true, data: [] } as IPlugin.ISearchResult<"music">;
+  }
+}
+
 async function search<T extends IMedia.SupportMediaType>(
   query: string,
   page: number,
   type: T,
 ) {
-  return type !== "music"
-    ? ({ isEnd: true, data: [] } as IPlugin.ISearchResult<"music">)
-    : await fetch(
-        buildUrl({
-          server: "netease",
-          type: "search",
-          id: "0",
-          keyword: query,
-        }),
-      )
-        .then((i) => i.json())
-        .then(
-          (i) =>
-            ({
-              isEnd: true,
-              data: (i as NanoMeting.SearchRsp).map((i) => ({
-                artist: i.artist,
-                title: i.name,
-                album: i.album,
-                artwork: i.pic,
-                url: i.url,
-                lrc: i.lrc,
-                platform: i.sourse,
-                id: new URL(i.url).searchParams.get("id"),
-              })),
-            }) as IPlugin.ISearchResult<"music">,
-        )
-        .catch(
-          () => ({ isEnd: true, data: [] }) as IPlugin.ISearchResult<"music">,
-        );
+  if (type !== "music") {
+    return { isEnd: true, data: [] } as IPlugin.ISearchResult<"music">;
+  }
+  const [{ data: a }, { data: b }] = await Promise.all([
+    searchByProvider(query, "netease"),
+    searchByProvider(query, "tencent"),
+  ]).catch(() => [
+    { isEnd: true, data: [] },
+    { isEnd: true, data: [] },
+  ]);
+  return {
+    isEnd: true,
+    data: Array.from({ length: Math.max(a.length, b.length) }).reduce(
+      (acc: IMusic.IMusicItem, _, i) => {
+        if (i < a.length) acc.push(a[i]);
+        if (i < b.length) acc.push(b[i]);
+        return acc;
+      },
+      [],
+    ),
+  } as IPlugin.ISearchResult<"music">;
 }
+
 export default {
   platform: "nano-meting",
   author: "Pizero",
   // TODO: update
   // srcUrl: "https://example.catcat.work/xxx.js",
-  // primaryKey: ["id", "aid", "bid"],
+  primaryKey: ["id", "platform"],
   cacheControl: "cache",
   version: "0.0.0",
   supportedSearchType: ["music"],
